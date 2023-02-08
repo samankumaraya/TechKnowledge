83d07ce49183bcfe9cd5c8beb722ce1acff8978d
Saman Kumara <samankumararajapaksha660@gmail.com>
2023-02-08 12:00:10 +0530

    add

--- Login.php
+++ Login.php
@@ -7,6 +7,20 @@
     <title>Log In</title>
 </head>
 <body>
-    
+    <form id="login" method="get" action="login.php">    
+        <label><b>User Name     
+        </b>    
+        </label>    
+        <input type="text" name="Uname" id="Uname" placeholder="Username">    
+        <br><br>    
+        <label><b>Password     
+        </b>    
+        </label>    
+        <input type="Password" name="Pass" id="Pass" placeholder="Password">    
+        <br><br>    
+        <input type="button" name="log" id="log" value="Log In Here">       
+        <br><br>     
+        Forgot <a href="#">Password</a>    
+    </form>  
 </body>
 </html>
