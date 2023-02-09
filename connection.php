053c7c564f06587ebc2a277ac7362f2517c3beee
Saman Kumara <samankumararajapaksha660@gmail.com>
2023-02-09 21:41:58 +0530

    modified

--- connection.php
+++ connection.php
@@ -12,6 +12,6 @@ $con= mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($con === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }else{
-    echo 'Connection successful';
+    
 }
 ?>
