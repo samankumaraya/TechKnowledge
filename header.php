a055d566e256f6a1c9ab44b0a8b804d0c65f4693
Saman Kumara <samankumararajapaksha660@gmail.com>
2023-02-19 00:11:35 +0530

    modified header

---
+++ header.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header</title>
+    <link rel="stylesheet" href="stylle.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        .container{
+    height: 100px;
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+   
+}
+.img{
+    margin-right: 170px;
+}
+
+a{
+    margin-top: 40px;
+    text-decoration: none;
+    margin-right: 30px;
+    margin-left: 0px;
+    font-size:20px;
+    
+}
+.aboutuss{
+    margin-right: 50px;
+}
+.img1{
+    height:50px;
+    margin-bottom: 40px;
+    margin-right: 20px;
+    
+}
+.a1{
+    margin-top:20px;
+}
+#cupboa{
+    margin-right:75px;
+}
+    </style>
+</head>
+<body>
+    <div class="container">
+        
+           <img src="logo.png"  class="img" alt="" srcset="">
+        
+           <a href="HomePage.php"><b>Home</b></a>
+           <a href="Login.php"><b>LogIn</b></a>
+           <a href="register.php"><b>Register</b></a>
+           <a href="aboutus.php" class="aboutuss"><b>About Us</b></a>
+           <a href="" class="a1"><img src="profile.png"  class="img1" alt="" srcset=""></a>
+           <a href="" class="a1"><img src="cupboard.png"  class="img1" id="cupboa" alt="" srcset=""></a>
+
+           
+
+       
+    </div>
+</body>
+</html>
