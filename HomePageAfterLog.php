ac1d802f2ef6000ea445bc04acaf266ad518b85e
Saman Kumara <samankumararajapaksha660@gmail.com>
2023-04-14 22:59:14 +0530

    modified

---
+++ HomePageAfterLog.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/homestyle.css">
+    <title>Home Page</title>
+    <style>
+       .cataheader{
+    margin-top: 5%;
+    color: blue;
+}
+
+.blogscatagories{
+    display: flex;
+    margin-top: 2%;
+    margin-left: 4%;
+   
+    
+}
+    </style>
+</head>
+<body>
+    <?php
+      include 'headerafterlog.php';
+    ?>
+    <div class="totalcontent">
+      
+    <div class="blogs">
+
+  <a href="">
+  <div class="catagory">
+    <img src="grade.png" alt="">
+    <h3>Title</h3>
+    </div>
+    
+  </a>
+    
+   <a href="">
+   <div class="catagory">
+    <img src="grade.png" alt="">
+    <h3>Title</h3>
+    </div>
+   </a>
+   
+<a href="">
+<div class="catagory">
+    <img src="grade.png" alt="">
+    <h3>Title</h3>
+    </div>
+</a>
+    
+<a href="">
+<div class="catagory">
+    <img src="grade.png" alt="">
+    <h3>Title</h3>
+    </div>
+</a>
+    </div>
+
+    <br>
+    <br>
+
+    <div class="blogs">
+
+  <a href="">
+  <div class="catagory">
+    <img src="grade.png" alt="">
+    <h3>Title</h3>
+    </div>
+  </a>
+    
+   <a href="">
+   <div class="catagory">
+    <img src="grade.png" alt="">
+    <h3>Title</h3>
+    </div>
+   </a>
+   
+<a href="">
+<div class="catagory">
+    <img src="grade.png" alt="">
+    <h3>Title</h3>
+    </div>
+</a>
+    
+<a href="">
+<div class="catagory">
+    <img src="grade.png" alt="">
+    <h3>Title</h3>
+    </div>
+</a>
+    </div>
+ 
+    <h2 class="cataheader" >Saman</h2>
+
+   <div class="blogscatagories">
+
+  <a href="">
+  <div class="catagory">
+    <img src="grade.png" alt="">
+    <h3>Title</h3>
+    </div>
+    
+  </a>
+    
+   <a href="">
+   <div class="catagory">
+    <img src="grade.png" alt="">
+    <h3>Title</h3>
+    </div>
+   </a>
+   
+<a href="">
+<div class="catagory">
+    <img src="grade.png" alt="">
+    <h3>Title</h3>
+    </div>
+</a>
+    
+<a href="">
+<div class="catagory">
+    <img src="grade.png" alt="">
+    <h3>Title</h3>
+    </div>
+</a>
+    </div>
+    
+
+    </div>
+
+    <?php
+      include 'footer.php';
+    ?>
+</body>
+</html>
---
+++ headerafterlog.php
@@ -0,0 +1,65 @@
+<!DOCTYPE htaml>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Tech Knowledge Academy</title>
+        <style>
+            @import url("https://fonts.googleapis.com/css2?family=Open+Sans&display=swap");
+body {
+    font-family: "Open Sans", sans-serif;
+    
+  
+    margin: 0;
+}
+header{
+    background-color: aquamarine;
+}
+a {
+    text-decoration: none;
+    color: #000;
+}
+a:hover {
+    color: rgb(179, 179, 179);
+}
+.site-header {
+    
+    padding: 0.5em 1em;
+    display: flex;
+    justify-content: space-between;
+}
+.site-identity h1 {
+    font-size: 1.5em;
+    margin: 0.6em 0;
+    display: inline-block;
+}
+.site-navigation ul,
+.site-navigation li {
+    margin: 0;
+    padding: 0;
+}
+.site-navigation li {
+    display: inline-block;
+    margin: 1.4em 1em 1em 1em;
+}
+        </style>
+    </head>
+    <body>
+        <header class="site-header">
+            <div class="site-identity">
+                <h1><a href="#">Tech Knowledge Academy</a></h1>
+            </div>
+            <nav class="site-navigation">
+                <ul class="nav">
+                    <li><a href="HomePage.php">Home</a></li>
+                    <li><a href="aboutus.php">About</a></li>
+                    <li><a href="https://www.blogger.com/blog/posts/7599516391469548502">Blog</a></li>
+                    <li><a href="contactus.php">Contact</a></li>
+                    <li><a href="">My Account</a></li>
+                    <li><a href="Login.php"></a></li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
